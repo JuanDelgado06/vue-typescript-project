e903229eb09a07da421c30b2af7bac55e164cec9
JuanDelgado06 <josedelgadozambrano30@gmail.com>
2020-05-02 15:37:44 -0500

    Problemas solucionados con mapState utilizando Typescript

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,11 +5,18 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: { 
+      id: '1df31d', 
+      name: 'Juanisimo Delgadiño',
+      categories: ['comida', 'ropa', 'programas']
+    },
+    owner: {
+      name: 'Juanisimo Delgadi',
+      age: 12
+    }
   },
   mutations: {
   },
   actions: {
   },
-  modules: {
-  }
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,5 +1,26 @@
 <template>
     <div>
-        <h1>Crear Evento</h1>
+        <h1>Crear Evento {{ user.name }} </h1>
+        <p v-for="(category, index) in user.categories" :key="index">{{category}}</p>
+        <h5>{{owner.name}}</h5>
+        <h2>{{username}}</h2>
     </div>
 </template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import {mapState} from 'vuex'
+
+@Component({
+    computed: mapState([
+        'user',
+        'owner'
+    ])
+})
+export default class EventCreate extends Vue {
+    user! : any;
+    public get username(): string {
+        return this.user.name
+    }
+}
+</script>

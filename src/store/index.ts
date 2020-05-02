129a870b97615c59cecc769ba6aa21c69917d28d
JuanDelgado06 <josedelgadozambrano30@gmail.com>
2020-05-02 16:33:28 -0500

    Ejemplo simple de state y getters terminado

--- src/store/index.ts
+++ src/store/index.ts
@@ -8,12 +8,33 @@ export default new Vuex.Store({
     user: { 
       id: '1df31d', 
       name: 'Juanisimo Delgadiño',
-      categories: ['comida', 'ropa', 'programas']
     },
+    categories: ['comida', 'ropa', 'programas'],
     owner: {
       name: 'Juanisimo Delgadi',
       age: 12
-    }
+    },
+    todos: [
+      {id: 1, text: '...', done: true},
+      {id: 2, text: '...', done: false},
+      {id: 3, text: '...', done: true},
+      {id: 4, text: '...', done: false}
+    ]
+  },
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    //De esta manera se pueden convinar getters
+    // activeTodosCount: (state, getters) => {
+    //   return state.todos.length - getters.doneTodos.length
+    // },
+    activeTodosCount: state => {
+      return state.todos.filter(todo => !todo.done).length
+    },
   },
   mutations: {
   },
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,26 +1,34 @@
 <template>
     <div>
         <h1>Crear Evento {{ user.name }} </h1>
-        <p v-for="(category, index) in user.categories" :key="index">{{category}}</p>
+        <p v-for="(category, index) in categories" :key="index">{{category}}</p>
         <h5>{{owner.name}}</h5>
         <h2>{{username}}</h2>
+        <h3>Cantidad de categorias {{catLength()}}</h3>
+        <h3>Cantidad de tareas completadas {{activeTodosCount}}</h3>
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
-import {mapState} from 'vuex'
+import {mapState, mapGetters} from 'vuex'
 
 @Component({
-    computed: mapState([
-        'user',
-        'owner'
-    ])
+    computed: {
+        ...mapState([ 'user', 'categories', 'owner' ]),
+        ...mapGetters(['activeTodosCount'])
+    }
 })
 export default class EventCreate extends Vue {
     user! : any;
     public get username(): string {
         return this.user.name
     }
+    public catLength() : number {
+        return this.$store.getters.catLength
+    }
+    // public compledTodos(): number {
+    //     return this.$store.getters.activeTodosCount
+    // }
 }
 </script>
